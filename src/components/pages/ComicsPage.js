6382577438a9ac01fc47f96c1bb3f29d7e860508
Alex <aleks742045@gmail.com>
2023-02-14 15:18:11 +0200

    finish project

---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,13 @@
+import ComicsList from '../comicsList/ComicsList';
+import AppBanner from '../appBanner/AppBanner';
+
+const Comicspage = () => {
+  return (
+    <>
+      <AppBanner />
+      <ComicsList />
+    </>
+  );
+};
+
+export default Comicspage;
---
+++ src/components/pages/index.js
@@ -0,0 +1,6 @@
+import Comicspage from './ComicsPage';
+import MainPage from './MainPage';
+import Page404 from './404';
+import SingleComicPage from './SingleComicPage';
+
+export { Comicspage, MainPage, Page404, SingleComicPage };
